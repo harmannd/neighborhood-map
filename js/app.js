8b8e25b44010c59ae107748ecd46a270847f0054
harmannd <derekhar2468@gmail.com>
2017-09-18 12:53:21 -0500

    Remodeled filter options to include filter by favorites

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ var googleSuccess = function() {
     var Location = function(data) {
         this.address = ko.observable(data.address);
         this.searchVisible = ko.observable(true);
-        this.favorite = ko.observable(false);
+        this.favorited = ko.observable(false);
     }
 
     var ViewModel = function() {
@@ -38,8 +38,11 @@ var googleSuccess = function() {
         var addressesSet = false;
 
         self.locationList = ko.observableArray([]);
-        this.search = ko.observable("");
-        this.open = ko.observable(false);
+        self.open = ko.observable(false);
+
+        self.searchValue = ko.observable("");
+        self.searchOptions = ko.observableArray(['Search', 'Favorites', 'Reset']);
+        self.chosenSearch = ko.observable('Search');
 
         initialAddresses.forEach(function(location) {
             mapFunctions.geocodeAddress(location.address);
@@ -47,15 +50,36 @@ var googleSuccess = function() {
         });
 
         self.filter = function() {
-            if (self.search() === "") {
+            if (self.chosenSearch() === self.searchOptions()[0]) {
+                self.search();
+            }
+            else if (self.chosenSearch() === self.searchOptions()[1]) {
+                self.favorites();
+            }
+            else {
+                self.reset();
+            }
+        };
+
+        self.search = function() {
+            if (self.searchValue() === "") {
                 self.reset();
             }
             else {
                 for (var i = 0; i < self.locationList().length; i++) {
-                    if (!self.locationList()[i].address().toUpperCase().includes(self.search().toUpperCase())) {
+                    if (!self.locationList()[i].address().toUpperCase().includes(self.searchValue().toUpperCase())) {
                         self.locationList()[i].searchVisible(false);
                         mapFunctions.hideMarker(i);
                     }
+                }
+            }
+        };
+
+        self.favorites = function() {
+            for (var i = 0; i < self.locationList().length; i++) {
+                if (!self.locationList()[i].favorited()) {
+                    self.locationList()[i].searchVisible(false);
+                    mapFunctions.hideMarker(i);
                 }
             }
         };
@@ -89,22 +113,22 @@ var googleSuccess = function() {
                     addressesSet = true;
                 }
             }
-        }
+        };
 
         self.infowindow = function() {
             mapFunctions.openInfoWindow(this.address());
-        }
+        };
 
-        self.favorite = function() {
-            if (this.favorite()) {
-                this.favorite(false);
+        self.favoriteLink = function() {
+            if (this.favorited()) {
+                this.favorited(false);
                 mapFunctions.unFavoriteMarker(this.address());
             }
             else {
-                this.favorite(true);
+                this.favorited(true);
                 mapFunctions.favoriteMarker(this.address());
             }
-        }
+        };
 
         mapFunctions.initMap(self);
     }
